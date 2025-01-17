8977a9f409e690e448bc6f1872d4aceb646e3d5a
networksaphyra <networksaphyra004@gmail.com>
2025-01-17 17:26:13 -0500

    fix: correct namings and add helper.

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -14,6 +14,11 @@ private:
   std::array<Bitboard, 64> queen_move_table;
   std::array<Bitboard, 64> king_move_table;
 
+  // Helper function
+  bool is_in_bounds(BOARD_FILE file, BOARD_RANK rank) {
+    return file >= FILE_A && file <= FILE_H && rank >= RANK_1 && rank <= RANK_8;
+  }
+
   void init_move_tables() { init_knight_move_table(); }
 
   // This feels very overengineered.
@@ -29,16 +34,16 @@ private:
         {2, -1}
     }};
 
-    for (SQUARE sq = static_cast<SQUARE>(0); sq < H8; sq = static_cast<SQUARE>(sq + 1)) {
+    for (SQUARE sq = A1; sq < H8; sq = static_cast<SQUARE>(sq + 1)) {
       Bitboard moves;
       BOARD_FILE file = get_file(sq);
       BOARD_RANK rank = get_rank(sq);
 
       for (const auto &[delta_file, delta_rank] : knight_deltas) {
         BOARD_FILE new_file = static_cast<BOARD_FILE>(static_cast<int>(file) + delta_file);
-        BOARD_RANK new_rank = static_cast<BOARD_RANK>(static_cast<int>(rank) + delta_file);
+        BOARD_RANK new_rank = static_cast<BOARD_RANK>(static_cast<int>(rank) + delta_rank);
 
-        if (new_file >= FILE_A && new_file <= FILE_H && new_rank >= RANK_1 && new_rank <= RANK_8) {
+        if (is_in_bounds(new_file, new_rank)) {
           SQUARE target = make_square(new_file, new_rank);
           moves.set_bit(target);
         }
