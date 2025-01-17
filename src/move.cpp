83c3fe501e745cb123b15c160c2f4cae3fa9f974
networksaphyra <networksaphyra004@gmail.com>
2025-01-17 01:03:28 -0500

    feat: add Move class to define a move.

---
+++ src/move.cpp
@@ -0,0 +1,24 @@
+#include "constants.cpp"
+#include <cstdint>
+
+class Move {
+private:
+  SQUARE origin;
+  SQUARE target;
+  uint16_t flags;
+
+public:
+  Move(SQUARE origin, SQUARE target, uint16_t flags = FLAG::QUIET_MOVE):
+  origin(origin),
+  target(target),
+  flags(flags) {}
+
+  SQUARE get_origin() { return origin; }
+  SQUARE get_target() { return target; }
+  uint16_t get_flags() { return flags; }
+
+  bool is_capture() { return flags & FLAG::CAPTURE; }
+  bool is_promotion() { return flags & FLAG::PAWN_PROMOTION; }
+  bool is_en_passant() { return flags & FLAG::EN_PASSANT; }
+  bool is_castling() { return flags & FLAG::CASTLING; }
+};
