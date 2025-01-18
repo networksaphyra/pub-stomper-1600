d24ab33aef3b43bd0b2fbe7f54174e643173fcc5
networksaphyra <networksaphyra004@gmail.com>
2025-01-17 23:47:23 -0500

    feat: add additional functionality for bit operations.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -1,4 +1,6 @@
+#pragma once
 #include <cstdint>
+#include "constants.cpp"
 
 class Bitboard {
 private:
@@ -7,10 +9,28 @@ private:
 public:
   Bitboard(): bitboard(0ULL) {}
 
-  bool is_occupied(int bit) { return (bitboard & (1ULL << bit)) != 0; }
+  bool is_occupied(int square) { return GET_AND(1ULL << square); }
   uint64_t get_bitboard() { return bitboard; }
+  int get_least_significant_bit() {
+    for (int i=0; i<64; ++i) {
+      if (is_occupied(i)) return i;
+    }
+    return -1;
+  }
 
   void set_bitboard(uint64_t bb) { bitboard = bb; }
   void set_bit(int bit) { bitboard |= (1ULL << bit); }
   void clear_bit(int bit) { bitboard &= ~(1ULL << bit); }
+
+  void OR(uint64_t other) { bitboard |= other; }
+  void OR(Bitboard other) { bitboard |= other.get_bitboard(); }
+  void AND(uint64_t other) { bitboard &= other; }
+  void AND(Bitboard other) { bitboard &= other.get_bitboard(); }
+  void NOT() { bitboard=~bitboard; }
+
+  uint64_t GET_OR(uint64_t other) { return bitboard | other; }
+  uint64_t GET_OR(Bitboard other) { return bitboard | other.get_bitboard(); }
+  uint64_t GET_AND(uint64_t other) { return bitboard & other; }
+  uint64_t GET_AND(Bitboard other) { return bitboard & other.get_bitboard(); }
+  uint64_t GET_NOT() { return ~bitboard; }
 };
