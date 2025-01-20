9bae2facea87c5f2b64e812b63fba43da1a79baf
networksaphyra <networksaphyra004@gmail.com>
2025-01-19 20:51:58 -0500

    feat: add XOR.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -7,7 +7,7 @@ private:
   uint64_t bitboard;
 
 public:
-  Bitboard(): bitboard(0ULL) {}
+  Bitboard(uint64_t bb = 0ULL): bitboard(bb) {}
 
   bool is_occupied(int square) { return GET_AND(1ULL << square); }
   uint64_t get_bitboard() { return bitboard; }
@@ -19,6 +19,7 @@ public:
   }
 
   void set_bitboard(uint64_t bb) { bitboard = bb; }
+  void set_bitboard(Bitboard bb) { bitboard = bb.get_bitboard(); }
   void set_bit(int bit) { bitboard |= (1ULL << bit); }
   void clear_bit(int bit) { bitboard &= ~(1ULL << bit); }
 
@@ -26,11 +27,15 @@ public:
   void OR(Bitboard other) { bitboard |= other.get_bitboard(); }
   void AND(uint64_t other) { bitboard &= other; }
   void AND(Bitboard other) { bitboard &= other.get_bitboard(); }
+  void XOR(uint64_t other) { bitboard ^= other; }
+  void XOR(Bitboard other) { bitboard ^= other.get_bitboard(); }
   void NOT() { bitboard=~bitboard; }
 
   uint64_t GET_OR(uint64_t other) { return bitboard | other; }
   uint64_t GET_OR(Bitboard other) { return bitboard | other.get_bitboard(); }
   uint64_t GET_AND(uint64_t other) { return bitboard & other; }
   uint64_t GET_AND(Bitboard other) { return bitboard & other.get_bitboard(); }
+  uint64_t GET_XOR(uint64_t other) { return bitboard ^ other; }
+  uint64_t GET_XOR(Bitboard other) { return bitboard ^ other.get_bitboard(); }
   uint64_t GET_NOT() { return ~bitboard; }
 };
