64ca360a76190170c6dd8ef91f0c3fe91adb30ef
networksaphyra <networksaphyra004@gmail.com>
2025-01-21 14:05:42 -0500

    feat: add king pseudo legal moves.

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -2,21 +2,13 @@
 #include "bitboard.cpp"
 #include "board.cpp"
 #include "move.cpp"
+#include "movetable.cpp"
 #include <array>
 #include <vector>
 
 class MoveGenerator {
 private:
-  std::array<Bitboard, 64> pawn_move_table;
-  std::array<Bitboard, 64> knight_move_table;
-  std::array<Bitboard, 64> bishop_move_table;
-  std::array<Bitboard, 64> rook_move_table;
-  std::array<Bitboard, 64> queen_move_table;
-  std::array<Bitboard, 64> king_move_table;
-
-  bool is_in_bounds(BOARD_FILE file, BOARD_RANK rank) {
-    return file >= FILE_A && file <= FILE_H && rank >= RANK_1 && rank <= RANK_8;
-  }
+  MoveTable move_table;
 
   Bitboard get_ray_attacks(SQUARE square, int delta_rank, int delta_file) {
     Bitboard attacks;
@@ -35,78 +27,19 @@ private:
     return attacks;
   }
 
-  void init_move_tables() {
-    init_knight_move_table();
-    init_bishop_move_table();
-    init_rook_move_table();
-    init_queen_move_table();
-  }
-
-  void init_knight_move_table() {
-    for (SQUARE square = A1; square <= H8; square = static_cast<SQUARE>(square + 1)) {
-      Bitboard moves;
-      BOARD_FILE file = get_file(square);
-      BOARD_RANK rank = get_rank(square);
-
-      for (const auto &[delta_rank, delta_file] : knight_deltas) {
-        BOARD_FILE new_file = static_cast<BOARD_FILE>(static_cast<int>(file) + delta_file);
-        BOARD_RANK new_rank = static_cast<BOARD_RANK>(static_cast<int>(rank) + delta_rank);
-
-        if (is_in_bounds(new_file, new_rank)) {
-          moves.set_bit(make_square(new_file, new_rank));
-        }
-      }
-
-      knight_move_table[square] = moves;
-    }
-  }
-
-  void init_bishop_move_table() {
-    for (SQUARE square = A1; square <= H8; square = static_cast<SQUARE>(square + 1)) {
-      Bitboard moves;
-      for (const auto &[delta_rank, delta_file] : bishop_deltas) {
-        moves.OR(get_ray_attacks(square, delta_rank, delta_file));
-      }
-      bishop_move_table[square] = moves;
-    }
-  }
-
-  void init_rook_move_table() {
-    const std::array<std::pair<int, int>, 4> rook_deltas = {{
-      {NORTH, 0}, {SOUTH, 0}, {0, EAST}, {0, WEST}
-    }};
-
-    for (SQUARE square = A1; square <= H8; square = static_cast<SQUARE>(square + 1)) {
-      Bitboard moves;
-      for (const auto &[delta_rank, delta_file] : rook_deltas) {
-        moves.OR(get_ray_attacks(square, delta_rank, delta_file));
-      }
-      rook_move_table[square] = moves;
-    }
-  }
-
-  void init_queen_move_table() {
-    for (SQUARE square = A1; square <= H8; square = static_cast<SQUARE>(square + 1)) {
-      queen_move_table[square].set_bitboard(
-        bishop_move_table[square].GET_OR(rook_move_table[square]));
-    }
-  }
-
-  std::vector<Move> generate_sliding_moves(Board &board, PIECE piece_type,
-                                         const std::vector<std::pair<int, int>> &directions,
-                                         const std::array<Bitboard, 64> &move_table) {
+  std::vector<Move> generate_sliding_moves(Board &board, PIECE piece) {
     std::vector<Move> moves;
     COLOR color = board.get_color();
-    Bitboard pieces = board.get_piece(color, piece_type);
+    Bitboard pieces = board.get_piece(color, piece);
     Bitboard friendly = board.get_color_occupied(color);
     Bitboard enemy = board.get_color_occupied(board.flip_color(color));
     Bitboard occupied = board.get_occupied();
 
     while (pieces.get_bitboard()) {
       SQUARE origin = static_cast<SQUARE>(pieces.get_least_significant_bit());
-      Bitboard moves_bb = move_table[origin];
+      Bitboard moves_bb = move_table.get_moves(piece, origin);
 
-      for (const auto &[delta_rank, delta_file] : directions) {
+      for (const auto &[delta_rank, delta_file] : directions[piece]) {
         Bitboard ray = get_ray_attacks(origin, delta_rank, delta_file);
         Bitboard blockers = ray.GET_AND(occupied);
 
@@ -129,27 +62,16 @@ private:
     return moves;
   }
 
-public:
-  MoveGenerator() {
-    init_move_tables();
-  }
-
-  // todo: filter illegal moves which put the king in check
-  std::vector<Move> generate_legal_moves(const Board &board) {
-    std::vector<Move> legal_moves;
-    return legal_moves;
-  }
-
-  std::vector<Move> generate_knight_moves(Board &board) {
+  std::vector<Move> generate_non_sliding_moves(Board &board, PIECE piece) {
     std::vector<Move> moves;
     COLOR color = board.get_color();
-    Bitboard knights = board.get_piece(color, PIECE::KNIGHT);
+    Bitboard piece_occupied = board.get_piece(color, piece);
     Bitboard friendly = board.get_color_occupied(color);
     Bitboard enemy = board.get_color_occupied(board.flip_color(color));
 
-    while (knights.get_bitboard()) {
-      SQUARE origin = static_cast<SQUARE>(knights.get_least_significant_bit());
-      Bitboard attack = knight_move_table[origin];
+    while (piece_occupied.get_bitboard()) {
+      SQUARE origin = static_cast<SQUARE>(piece_occupied.get_least_significant_bit());
+      Bitboard attack = move_table.get_moves(piece, origin);
       attack.AND(friendly.GET_NOT());
 
       while (attack.get_bitboard()) {
@@ -157,20 +79,35 @@ public:
         moves.push_back(Move(origin, target, enemy.is_occupied(target) ? FLAG::CAPTURE : FLAG::NONE));
         attack.clear_bit(target);
       }
-      knights.clear_bit(origin);
+      piece_occupied.clear_bit(origin);
     }
     return moves;
   }
 
+public:
+  // todo: correctly classify pseudo legal move
+  std::vector<Move> generate_legal_moves(const Board &board) {
+    std::vector<Move> legal_moves;
+    return legal_moves;
+  }
+
+  std::vector<Move> generate_knight_moves(Board &board) {
+    return generate_non_sliding_moves(board, PIECE::KNIGHT);
+  }
+
   std::vector<Move> generate_bishop_moves(Board &board) {
-    return generate_sliding_moves(board, PIECE::BISHOP, bishop_deltas, bishop_move_table);
+    return generate_sliding_moves(board, PIECE::BISHOP);
   }
 
   std::vector<Move> generate_rook_moves(Board &board) {
-    return generate_sliding_moves(board, PIECE::ROOK, rook_deltas, rook_move_table);
+    return generate_sliding_moves(board, PIECE::ROOK);
   }
 
   std::vector<Move> generate_queen_moves(Board &board) {
-    return generate_sliding_moves(board, PIECE::QUEEN, queen_deltas, queen_move_table);
+    return generate_sliding_moves(board, PIECE::QUEEN);
+  }
+
+  std::vector<Move> generate_king_moves(Board &board) {
+    return generate_non_sliding_moves(board, PIECE::KING);
   }
 };
