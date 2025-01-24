b99d7374c2a99120a7bff07a74e2ae15d6eec7d4
networksaphyra <networksaphyra004@gmail.com>
2025-01-24 01:37:00 -0500

    refactor: change evaluate.

--- include/evaluate.h
+++ include/evaluate.h
@@ -1,11 +1,100 @@
+// very basic evaluation function.
+// in the future, i might decide to work on this further.
+
+#pragma once
+
 #include <array>
-#include <vector>
+#include "constants.h"
 #include "board.h"
 
-class Evaluation {
-public:
-  Evaluation();
-  int evaluate(Board& board);
+class Evaluate {
+  private:
+  const std::array<int, 6> PIECE_VALUE {
+    100,
+    310,
+    320,
+    500,
+    900,
+    20000
+  };
 
-private:
+  // this double curly brace thing with STL arrays is so dumb istg.
+  // NOTE: since the tables are symmetric across the Y-axis, we can treat bottom-right as A1 since it's easier to do math with it
+  const std::array<std::array<int, 64>, 6> PIECE_SQUARE_TABLE = {{
+    // pawn
+   {{
+      0,  0,  0,  0,  0,  0,  0,  0,
+     50, 50, 50, 50, 50, 50, 50, 50,
+     10, 10, 20, 30, 30, 20, 10, 10,
+      5,  5, 10, 25, 25, 10,  5,  5,
+      0,  0,  0, 20, 20,  0,  0,  0,
+      5, -5,-10,  0,  0,-10, -5,  5,
+      5, 10, 10,-20,-20, 10, 10,  5,
+      0,  0,  0,  0,  0,  0,  0,  0
+    }},
+    // knight
+    {{
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50
+    }},
+    {{
+      // bishop
+      -20,-10,-10,-10,-10,-10,-10,-20,
+      -10,  0,  0,  0,  0,  0,  0,-10,
+      -10,  0,  5, 10, 10,  5,  0,-10,
+      -10,  5,  5, 10, 10,  5,  5,-10,
+      -10,  0, 10, 10, 10, 10,  0,-10,
+      -10, 10, 10, 10, 10, 10, 10,-10,
+      -10,  5,  0,  0,  0,  0,  5,-10,
+      -20,-10,-10,-10,-10,-10,-10,-20,
+    }},
+    {{
+      // rook
+        0,  0,  0,  0,  0,  0,  0,  0,
+        5, 10, 10, 10, 10, 10, 10,  5,
+       -5,  0,  0,  0,  0,  0,  0, -5,
+       -5,  0,  0,  0,  0,  0,  0, -5,
+       -5,  0,  0,  0,  0,  0,  0, -5,
+       -5,  0,  0,  0,  0,  0,  0, -5,
+       -5,  0,  0,  0,  0,  0,  0, -5,
+        0,  0,  0,  5,  5,  0,  0,  0
+    }},
+    {{
+      // queen
+      -20,-10,-10, -5, -5,-10,-10,-20,
+      -10,  0,  0,  0,  0,  0,  0,-10,
+      -10,  0,  5,  5,  5,  5,  0,-10,
+       -5,  0,  5,  5,  5,  5,  0, -5,
+        0,  0,  5,  5,  5,  5,  0, -5,
+      -10,  5,  5,  5,  5,  5,  0,-10,
+      -10,  0,  5,  0,  0,  0,  0,-10,
+      -20,-10,-10, -5, -5,-10,-10,-20
+    }},
+    {{
+      // king
+      -30,-40,-40,-50,-50,-40,-40,-30,
+      -30,-40,-40,-50,-50,-40,-40,-30,
+      -30,-40,-40,-50,-50,-40,-40,-30,
+      -30,-40,-40,-50,-50,-40,-40,-30,
+      -20,-30,-30,-40,-40,-30,-30,-20,
+      -10,-20,-20,-20,-20,-20,-20,-10,
+       20, 20,  0,  0,  0,  0, 20, 20,
+       20, 30, 10,  0,  0, 10, 30, 20
+    }}
+  }};
+
+  int get_square_value(PIECE piece, SQUARE square) {
+    return PIECE_SQUARE_TABLE[piece][square];
+  }
+
+  public:
+  int get_piece_value(PIECE piece);
+  int get_piece_square_value(COLOR color, PIECE piece, SQUARE square);
+  int evaluate(Board& board);
 };
--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1,111 +1,37 @@
 #include "../include/evaluate.h"
 
-const std::array<int, 6> PIECE_VALUES {
-  100,
-  320,
-  330,
-  500,
-  900,
-  20000
+int Evaluate::get_piece_value(PIECE piece) {
+  if (piece == PIECE::NO_PIECE) return 0;
+  return PIECE_VALUE[piece];
 };
 
-int PIECE_SQUARE_TABLES[2][6][64] = {
-  {
-  // Note: All these tables have A1 as bottom left, so we're gonna have to do some math when we use them.
-  // Order of tables: PAWN, KNIGHT, BISHOP, ROOK, QUEEN
-  // Also, mirror these values for BLACK
-    {
-      0,  0,  0,  0,  0,  0,  0,  0,
-      50, 50, 50, 50, 50, 50, 50, 50,
-      10, 10, 20, 30, 30, 20, 10, 10,
-      5,  5, 10, 25, 25, 10,  5,  5,
-      0,  0,  0, 20, 20,  0,  0,  0,
-      5, -5,-10,  0,  0,-10, -5,  5,
-      5, 10, 10,-20,-20, 10, 10,  5,
-      0,  0,  0,  0,  0,  0,  0,  0
-    },
-    {
-      -50,-40,-30,-30,-30,-30,-40,-50,
-      -40,-20,  0,  0,  0,  0,-20,-40,
-      -30,  0, 10, 15, 15, 10,  0,-30,
-      -30,  5, 15, 20, 20, 15,  5,-30,
-      -30,  0, 15, 20, 20, 15,  0,-30,
-      -30,  5, 10, 15, 15, 10,  5,-30,
-      -40,-20,  0,  5,  5,  0,-20,-40,
-      -50,-40,-30,-30,-30,-30,-40,-50,
-    },
-    {
-      -20,-10,-10,-10,-10,-10,-10,-20,
-      -10,  0,  0,  0,  0,  0,  0,-10,
-      -10,  0,  5, 10, 10,  5,  0,-10,
-      -10,  5,  5, 10, 10,  5,  5,-10,
-      -10,  0, 10, 10, 10, 10,  0,-10,
-      -10, 10, 10, 10, 10, 10, 10,-10,
-      -10,  5,  0,  0,  0,  0,  5,-10,
-      -20,-10,-10,-10,-10,-10,-10,-20,
-    },
-    {
-      0,  0,  0,  0,  0,  0,  0,  0,
-      5, 10, 10, 10, 10, 10, 10,  5,
-      -5,  0,  0,  0,  0,  0,  0, -5,
-      -5,  0,  0,  0,  0,  0,  0, -5,
-      -5,  0,  0,  0,  0,  0,  0, -5,
-      -5,  0,  0,  0,  0,  0,  0, -5,
-      -5,  0,  0,  0,  0,  0,  0, -5,
-      0,  0,  0,  5,  5,  0,  0,  0
-    },
-    {
-      -20,-10,-10, -5, -5,-10,-10,-20,
-      -10,  0,  0,  0,  0,  0,  0,-10,
-      -10,  0,  5,  5,  5,  5,  0,-10,
-        -5,  0,  5,  5,  5,  5,  0, -5,
-        0,  0,  5,  5,  5,  5,  0, -5,
-      -10,  5,  5,  5,  5,  5,  0,-10,
-      -10,  0,  5,  0,  0,  0,  0,-10,
-      -20,-10,-10, -5, -5,-10,-10,-20
-    },
-    {
-      -30,-40,-40,-50,-50,-40,-40,-30,
-      -30,-40,-40,-50,-50,-40,-40,-30,
-      -30,-40,-40,-50,-50,-40,-40,-30,
-      -30,-40,-40,-50,-50,-40,-40,-30,
-      -20,-30,-30,-40,-40,-30,-30,-20,
-      -10,-20,-20,-20,-20,-20,-20,-10,
-        20, 20,  0,  0,  0,  0, 20, 20,
-        20, 30, 10,  0,  0, 10, 30, 20
-    }
-  },
-  {}
-};
+// for black, the square at a8 should be equivalent to a1
+// and the value of the square should be reversed
 
-Evaluation::Evaluation() {
-  for (PIECE piece = PIECE::PAWN; piece <= PIECE::KING; piece = static_cast<PIECE>(piece + 1)) {
-    for (SQUARE square = A1; square <= H8; square = static_cast<SQUARE>(square + 1)) {
-      PIECE_SQUARE_TABLES[COLOR::BLACK][piece][square] = PIECE_SQUARE_TABLES[COLOR::WHITE][piece][63 - square];
-    }
+int Evaluate::get_piece_square_value(COLOR color, PIECE piece, SQUARE square) {
+  SQUARE table_square = static_cast<SQUARE>(63 - square);
+
+  if (color == COLOR::BLACK) {
+    BOARD_RANK rank = get_rank(table_square);
+    table_square = make_square(get_file(table_square), static_cast<BOARD_RANK>(7 - rank));
   }
+
+  int square_value = get_square_value(piece, table_square);
+  return square_value * (color == COLOR::WHITE ? 1 : -1);
 }
 
-int Evaluation::evaluate(Board& board) {
-  int score = 0;
-  COLOR perspective = board.get_color();
-  COLOR opposite = board.flip_color(perspective);
+int Evaluate::evaluate(Board& board) {
+  int eval = 0;
 
-  for (int color = COLOR::WHITE; color <= COLOR::BLACK; ++color) {
-    int color_multiplier = (color == perspective) ? 1 : -1;
-    for (int piece = PIECE::PAWN; piece <= PIECE::KING; ++piece) {
-      Bitboard piece_bb = board.get_piece(static_cast<COLOR>(color), static_cast<PIECE>(piece));
-      while (piece_bb.get_bitboard()) {
+  for (COLOR color = COLOR::WHITE; color <= COLOR::BLACK; color = static_cast<COLOR>(color + 1)) {
+    for (PIECE piece = PIECE::PAWN; piece <= PIECE::KING; piece = static_cast<PIECE>(piece + 1)) {
+      Bitboard piece_bb = board.get_piece(color, piece);
+      while (piece_bb.count_bits()) {
         SQUARE square = static_cast<SQUARE>(piece_bb.pop_least_significant_bit());
-
-        score += color_multiplier * PIECE_VALUES[piece];
-
-        score += color_multiplier * PIECE_SQUARE_TABLES[color][piece][
-          color == COLOR::WHITE ? square : (63 - square)
-        ];
+        eval += get_piece_square_value(color, piece, square);
       }
     }
   }
 
-  return score;
+  return eval;
 }
